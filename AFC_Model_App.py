d2a34bcb0c138231498f98c912cba0e848d8c823
matt-brzozow <matt.brzozow@gmail.com>
2022-05-08 16:18:30 -0400

    commit

--- AFC_Model_App.py
+++ AFC_Model_App.py
@@ -1278,9 +1278,12 @@ with st.expander("Spider Diagram"):
     with open(fig_contam_spider_DL, "rb") as img:
         btn = st.download_button(label = "Download Contamination Spider Plot", data = img, file_name = fig_contam_spider_DL, mime = "image/svg")
 
+st.write("-----------------------")
+st.write("")
+
 #####################################################################################################
 # ASSIMILATION FRACTIONAL CRYSTALLIZATION
 #####################################################################################################
 
-st.header("Rayleigh Fractional Crystallization")
+st.header("Assimilation Fractional Crystallization")
 st.write("Coming Soon")
